ff4e55de0d96b5b9d760019e0c7200f885ec09b6
leedong <11046083@ntub.edu.tw>
2023-12-17 23:09:59 +0800

    移除 - 多餘註解、修改ReadMe.md

--- ReadMe.md
+++ ReadMe.md
@@ -1,1 +1,12 @@
-test
+# iot-backend 
+物聯網專題 - Django專案檔
+
+### 套件安裝
+
+```http
+pip install mysqlclient
+pip install line-bot-sdk
+pip install django-bootstrap-datepicker-plus
+pip install timeout-decorator
+pip install opencv-python
+```
--- iotApp/veiws/hours.py
+++ iotApp/veiws/hours.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
-from django.db.models import Sum  # 引入Sum聚合函數
+from django.db.models import Sum
 from iotApp.middleware import login_required
 from ..models import sign
 
 @login_required
 def hours(request):
-    # 使用annotate和Sum計算每個學號的時數加總
     late_attendance_records = sign.objects.values('student__student_id') \
         .annotate(total_hours=Sum('hours'))
 
